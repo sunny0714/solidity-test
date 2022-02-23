366bf8162c5925f649d49db27278809fbf530efe
sunny0714 <sun2020victory@gmail.com>
2022-02-23 11:05:10 +0200

    hello world

---
+++ contracts/01-hello-world/HelloWorld.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title HelloWorld
+ * @dev This contract implements a function which prints "Hello World!"
+ * This is the entry point in the world of smart contract development
+ */
+
+contract HelloWorld {
+
+    /**
+     * @dev Prints "Hello World!"
+     */
+    
+    function print() public pure returns (string memory){
+        return "Hello World!";
+    }
+}
