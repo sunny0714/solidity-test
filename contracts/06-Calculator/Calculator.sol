2b84cf73071ac7c1aebc1e81e42857007d158ebe
sunny0714 <sun2020victory@gmail.com>
2022-02-25 13:08:28 +0200

    calculator

---
+++ contracts/06-Calculator/Calculator.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Calculator {
+
+    /**
+     * @dev Adds two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
+    function add(int a, int b) public pure returns (int){
+        return (a+b);
+    }
+
+    /**
+     * @dev Subtracts two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
+    function sub(int a, int b) public pure returns (int){
+        return (a-b);
+    }
+
+    /**
+     * @dev Multiplies two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
+    function mul(int a, int b) public pure returns (int){
+        return (a*b);
+    }
+
+    /**
+     * @dev Divides two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
+    function div(int a, int b) public pure returns (int){
+        return (a/b);
+    }
+}
