c714018dabf390a93293c39d3fbb4a5c58df73d4
sunny0714 <sun2020victory@gmail.com>
2022-02-25 12:03:54 +0200

    Greeter

---
+++ contracts/03-Greeter/Greeter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Greeter
+ * @dev This contract sets greeting string and returns it on user demand
+ */
+
+contract Greeter {
+
+    string private _greeting;
+
+    /**
+     * @dev Sets greeting message
+     * @param message greeting message by user
+     */
+
+    function greet(string memory message) public {
+        _greeting = message;
+    }
+
+    /**
+     * @dev Returns greeting message
+     */
+
+    function getGreeting() public view returns (string memory){
+        return _greeting;
+    }
+}
