359a8f03c2d22848495b1601d4582dcc6bf7efda
sunny0714 <sun2020victory@gmail.com>
2022-02-26 17:54:06 +0200

    ThreeAndSeven

---
+++ contracts/07-ThreeAndSeven/ThreeAndSeven.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract ThreeAndSeven {
+
+    /**
+     * @dev Tells whether number is divisible by 3 or 7 and greater than 10
+     * @param num Number to check
+     */
+
+    function check(int num) public pure returns (bool){
+        if((num % 3 == 0 || num % 7 == 0) && num > 10)
+            return true;
+        return false;
+    }
+}
