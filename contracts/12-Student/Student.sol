66fd91e5a363b40c4fef130093e5f24f9b03372f
sunny0714 <sun2020victory@gmail.com>
2022-02-28 06:23:27 +0200

    Student

---
+++ contracts/12-Student/Student.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Student {
+    mapping(uint => string) public students; // rollnumber => name
+
+    event Enrolled(uint rollnumber);
+
+     /**
+     * @dev Enrolls student
+     * @param rollnumber Roll number of student
+     * @param name Name of student
+     */
+
+    function enroll(uint rollnumber, string memory name) public {
+        students[rollnumber] = name;
+        emit Enrolled(rollnumber);
+    }
+
+}
